83b7cc383d75b238635899e984f6cd6cfc5f129c
niranjanece <114294125+niranjanece@users.noreply.github.com>
2022-11-28 18:01:53 +0530

    Create StringMethods.java

---
+++ java/StringMethods.java
@@ -0,0 +1,14 @@
+public class Main
+{
+	public static void main(String[] args) {
+		String fname = "Ramesh";
+		String sname = "Babu";
+		System.out.println(fname.toUpperCase());
+		System.out.println(sname.toLowerCase());
+		System.out.println(fname.concat(sname));
+		System.out.println(fname.length());
+		System.out.println(fname.charAt(4));
+		System.out.println(fname.contains("e"));
+		System.out.println(sname.isEmpty());
+	}
+}
