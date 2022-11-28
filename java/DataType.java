0981ced8849d14777ebac4a6431309d47e5dd70f
niranjanece <114294125+niranjanece@users.noreply.github.com>
2022-11-28 17:51:52 +0530

    Create DataType.java

---
+++ java/DataType.java
@@ -0,0 +1,16 @@
+
+public class Main
+{
+	public static void main(String[] args) {
+		int num = 5;             
+		float flo = 5.99f;    
+        char letter = 'D';         
+        boolean bool = true;       
+        String text = "Hello"; 
+        System.out.println(num);
+        System.out.println(flo);
+        System.out.println(letter);
+        System.out.println(bool);
+        System.out.println(text);
+	}
+}
